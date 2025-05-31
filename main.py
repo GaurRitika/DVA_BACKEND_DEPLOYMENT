746f28cafe521e6d9287994b35206e325b828fd0
kuritika098 <ritika525.hd@gmail.com>
2025-05-31 15:28:09 +0530

    changed main.py

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 # backend/main.py
+import os
 from app import app
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8080)
+    port = int(os.environ.get("PORT", 8080))
+    uvicorn.run(app, host="0.0.0.0", port=port)
