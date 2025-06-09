ee39b2e1c88b3df61830161a61a145cf5725cce6
kuritika098 <ritika525.hd@gmail.com>
2025-06-09 23:58:01 +0530

    fianllly

--- app/__init__.py
+++ app/__init__.py
@@ -15,7 +15,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=["http://localhost:3000" ,
                 #    'https://frontend-dot-ayurguide-451219.uc.r.appspot.com' 
-                "https://mydva-deployment-5wkf7skpp-ritika-gaurs-projects.vercel.app/"
+                "https://mydva-deployment-5wkf7skpp-ritika-gaurs-projects.vercel.app"
                    ],
     allow_credentials=True,
     allow_methods=["*"],
